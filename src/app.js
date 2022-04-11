72c6b8c881d60eb631f5760a047b4882837b8e12
atxoupi <andres.hermelo@gmail.com>
2022-04-11 09:56:09 +0000

    Excuses

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#Excuse").innerHTML = randomExcuse();
+};
+
+let randomExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoidx = Math.floor(Math.random() * who.length);
+  let whatidx = Math.floor(Math.random() * what.length);
+  let whenidx = Math.floor(Math.random() * when.length);
+
+  return who[whoidx] + " " + what[whatidx] + " " + when[whenidx] + " ";
 };
